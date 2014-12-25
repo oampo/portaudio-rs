31450a31a213a9678debdb3fcd7033e3e0c9b9be
travis-ci <travis@travis-ci.org>
2014-12-25 14:59:12 +0000

    Auto doc upload from travis

--- implementors/core/fmt/trait.Show.js
+++ implementors/core/fmt/trait.Show.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['portaudio'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Show.html' title='core::fmt::Show'>Show</a> for <a class='enum' href='portaudio/pa/enum.PaError.html' title='portaudio::pa::PaError'>PaError</a>",];
+implementors['portaudio'] = ["<a class='stability Unstable' title='Unstable: I/O and core have yet to be reconciled'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Show.html' title='core::fmt::Show'>Show</a> for <a class='enum' href='portaudio/pa/enum.PaError.html' title='portaudio::pa::PaError'>PaError</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
